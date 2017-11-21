2a1b3e9434e98c422dd61871781db19014fd4cfb
Cinan Rakosnik <cinan6@gmail.com>
2017-11-21 16:24:20 +0100

    Don't rehydrate

--- src/updater.js
+++ src/updater.js
@@ -73,18 +73,6 @@ const offlineUpdater = function offlineUpdater(
     };
   }
 
-  if (action.type === PERSIST_REHYDRATE) {
-    return {
-      ...state,
-      ...action.payload.offline,
-      online: state.online,
-      netInfo: state.netInfo,
-      retryScheduled: initialState.retryScheduled,
-      retryCount: initialState.retryCount,
-      busy: initialState.busy
-    };
-  }
-
   if (action.type === OFFLINE_SCHEDULE_RETRY) {
     return {
       ...state,
